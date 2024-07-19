a1e2f47a5b9b2bcc0ae73f336fd025bdfe2da06d
DaianaOdorizzi <d.lealodorizzi@gmail.com>
2024-07-19 14:59:36 -0300

    Exemplos controle de fluxo

---
+++ controle-fluxo/src/ExemploForArray.java
@@ -0,0 +1,11 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos[] = { "JOÃO", "MARIA", "ANA", "FELIPE" };
+
+	    for (int x=0; x<alunos.length; x++) {
+
+		    System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+
+        }
+    }
+}
